4e8d4481f0aeba43beeca0d97d83b42f5224e982
Janell.Huyck <Janell.Huyck@outlook.com>
2020-07-06 16:57:19 -0400

    created user model, registered it in admin, created helpers.py and makenumber() for creating a library card number

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from custom_user.models import CustomUser
 
-# Register your models here.
+
+# help in this section is from
+# https://testdriven.io/blog/django-custom-user-model/
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('display_name', 'email', 'library_card_number',
+                    'signup_date', 'is_admin')
+    list_filter = ('display_name', 'email', 'library_card_number',
+                   'signup_date', 'is_admin')
+    fieldsets = (
+        (None, {'fields': ('display_name', 'email', 'library_card_number',
+                           'signup_date', 'is_admin')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- custom_user/models.py
+++ custom_user/models.py
@@ -1,14 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .helpers import makenumber
+
+
+class CustomUser(AbstractUser):
+    library_card_number = models.PositiveIntegerField(null=True, blank=True)
+    email = models.EmailField(max_length=254)
+    signup_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    display_name = models.CharField(max_length=50, unique=True)
+    REQUIRED_FIELDS = ['email', 'display_name']
+
+    def __str__(self):
+        return self.display_name
+
+    def save(self, *args, **kwargs):
+        if not self.library_card_number:
+            self.library_card_number = makenumber()
+        super(CustomUser, self).save(**args, **kwargs)
 
 
 """
-User: CharField
+Custom_User
 
+display_name: charfield
 library card number : integer field
 password :  Charfield, password widget
 signup date: datefield
 email: email field
-Age: float
 Is an admin: Boolean
 
 """
