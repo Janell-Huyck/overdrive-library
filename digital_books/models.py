13f4ac0c1fcf6ef50114a3c7a8f6c49cb467b98a
Michael Gabbard <zanvoysan@gmail.com>
2020-07-06 20:16:49 -0400

    book model, checkout and holds still need work

--- digital_books/models.py
+++ digital_books/models.py
@@ -1,14 +1,30 @@
 from django.db import models
-
+from custom_user.models import CustomUser
 """
 Books
 
 Title : charfield
+limit: 3
 URL : URL link field
 Description : text
 author: Charfield
 Who has it on hold: one to many
 Who has it checked out -one to many field
-limit: 3
 
 """
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    limit = models.IntegerField(default = 3)
+    checked_out = models.ManyToManyField(CustomUser, related_name='checked_out')
+    holds = models.ManyToManyField(CustomUser, related_name='holds')
+    # , through='HoldOrder'
+    URL = models.URLField(max_length=200)
+
+
+class HoldOrder(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
