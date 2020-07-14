74df06e16aaa0aa34ab50085cbff6b3e6c72b3f8
Janell.Huyck <Janell.Huyck@outlook.com>
2020-07-14 13:39:52 -0400

    runserver with --insecure to show css

--- library_project/settings.py
+++ library_project/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -124,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticdeploy/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticdeploy")
 
