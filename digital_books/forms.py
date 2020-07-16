a0bcd6e418711be97345bcb393a149006f344bb6
Janell.Huyck <Janell.Huyck@outlook.com>
2020-07-16 15:20:01 -0400

    added widget for placeholder text in add-book form

--- digital_books/forms.py
+++ digital_books/forms.py
@@ -7,3 +7,9 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ['title', 'author_last',
                   'author_first', 'description', 'URL', 'language']
+        widgets = {
+            'author_last': forms.TextInput(
+                attrs={'placeholder': 'Last Name'}),
+            'author_first': forms.TextInput(
+                attrs={'placeholder': 'First Name'}),
+        }
